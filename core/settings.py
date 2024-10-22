cabd72a0971f5787ad29951e80ca5d00fb29638d
hooman182 <hoomanm87@gmail.com>
2024-10-22 11:26:57 +0330

    feat: Create order view & order admin model & order templates

--- core/settings.py
+++ core/settings.py
@@ -41,7 +41,13 @@ INSTALLED_APPS = [
     'shop.apps.ShopConfig',
     'cart.apps.CartConfig',
     'orders.apps.OrdersConfig',
+    
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from cart.cart import Cart
+from orders.forms import OrderCreateForm
+from orders.models import OrderItem
 
-# Create your views here.
+
+def order_create(request):
+    cart = Cart(request)
+    if request.method == 'POST':
+        order_form = OrderCreateForm(request.POST)
+        if order_form.is_valid():
+            order = order_form.save()
+            for item in cart:
+                OrderItem.objects.create(
+                    order=order, product=item['product'], price=item['price'], quantity=item['quantity'])
+                cart.clean()
+
+            return render(request, 'orders/order_success.html', {'order': order})
+    else:
+        order_form = OrderCreateForm()
+            
+    return render(request, 'orders/order_create.html', {'cart': cart, 'order_form': order_form})
