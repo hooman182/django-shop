7c26b8a5761e7e45f6c753b87574ddc763729e74
hooman <hoomanm87@gmail.com>
2025-10-27 00:04:09 +0330

    remove category model and admin config from shop app

--- shop/admin.py
+++ shop/admin.py
@@ -1,15 +1,12 @@
 from django.contrib import admin
-from shop.models import Category, Product
+from shop.models import Product
 
+# -------------------------------------------------------
 
-@admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = ['name', 'slug']
-    prepopulated_fields = {'slug': ('name',)}
-    
-@admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['name', 'price', 'available', 'slug', 'created', 'updated']
     list_filter = ['available', 'created', 'updated', 'price']
     list_editable = ['price', 'available']
     prepopulated_fields = {'slug': ('name',)}
+    
+admin.site.register(Product, ProductAdmin)
--- shop/models.py
+++ shop/models.py
@@ -1,27 +1,12 @@
 from django.db import models
 from django.urls import reverse
+from category.models import Category
 
-
-class Category(models.Model):
-    name = models.CharField(max_length=200)
-    slug = models.SlugField(max_length=200, unique=True)
-
-    class Meta:
-        ordering = ['name']
-        indexes = [models.Index(fields=['name'])]
-        verbose_name = 'category'
-        verbose_name_plural = 'categories'
-
-    def get_absolute_url(self):
-        return reverse('shop:product_list_by_category', args=[self.slug])
-
-    def __str__(self) -> str:
-        return self.name
-
-
+# -------------------------------------------------------
+        
 class Product(models.Model):
     name = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250, unique=True, allow_unicode=True)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=3)
     image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
