e0c63f14f394e53e1acc7c1bbf2bb09414924fd7
hooman <hoomanm87@gmail.com>
2025-11-13 18:50:18 +0330

    feat: customize admin site titles

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static 
+from django.contrib import admin
 
 
 urlpatterns = [
@@ -13,3 +14,8 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
+
+admin.site.site_title = "Admin panel"
+admin.site.site_header = "ShopGo Admin Panel"
+admin.site.index_title = "ShopGo"
