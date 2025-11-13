3803c25a020b51a99cb14c76bc86efe6297366f1
hooman <hoomanm87@gmail.com>
2025-11-13 18:47:39 +0330

    feat: add order model fields to form with presian labels

--- orders/forms.py
+++ orders/forms.py
@@ -3,6 +3,15 @@ from orders.models import Order
 
 
 class OrderCreateForm(forms.ModelForm):
+    first_name = forms.CharField(max_length=50, label='نام')
+    last_name = forms.CharField(max_length=50, label='نام خانوادگی')
+    email = forms.EmailField(label='ایمیل')
+    province = forms.CharField(max_length=50, label='استان')
+    city = forms.CharField(max_length=50, label='شهر')
+    address = forms.CharField(max_length=100, label='آدرس')
+    postal_code = forms.CharField(max_length=20, label='کد پستی')
+    description = forms.CharField(label='توضیحات', widget=forms.Textarea(attrs={'rows': 4}))
+    
     class Meta:
         model = Order
-        fields = ['first_name', 'last_name', 'email', 'address', 'postal_code', 'city']
+        fields = ['first_name', 'last_name', 'email', 'province', 'city', 'address','postal_code', 'description']
